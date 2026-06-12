017fe2d38460ec424287d78581e4cf7e65056eb2
isabellemeredith <isabellejmeredith@gmail.com>
2026-06-12 11:43:53 -0600

    Decided that a glmm is more appropriate here.

--- statistical_analysis.py
+++ statistical_analysis.py
@@ -4,6 +4,11 @@ import sqlite3
 import plotly.express as px
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
+from statsmodels.genmod.bayes_mixed_glm import BinomialBayesMixedGLM
+import gpboost as gpb
+import scipy.stats as stats
+import numpy as np
+
 
 import utils.display as display
 
@@ -15,13 +20,14 @@ if __name__ == "__main__":
     analysis_view_query = """
         CREATE VIEW analysis 
         AS
-        SELECT population_id, POPULATION.sample_id, POPULATION.sample_id_text, population_name, SUM(count) OVER (PARTITION BY POPULATION.sample_id) AS total_count, count, 1.0 * count / SUM(count) OVER (PARTITION BY POPULATION.sample_id) AS percentage, condition, response, sample_type, time_from_treatment_start, age, sex
+        SELECT population_id, POPULATION.sample_id, POPULATION.sample_id_text, population_name, SUM(count) OVER (PARTITION BY POPULATION.sample_id) AS total_count, count, 1.0 * count / SUM(count) OVER (PARTITION BY POPULATION.sample_id) AS percentage, condition, response, sample_type, treatment, time_from_treatment_start, age, sex
         FROM (POPULATION JOIN SAMPLE 
         ON POPULATION.sample_id = SAMPLE.sample_id
         JOIN SUBJECT
         ON SAMPLE.subject_id = SUBJECT.subject_id) 
         WHERE sample_type = "PBMC"
         AND condition = "melanoma"
+        AND treatment = "miraclib"
         AND response IS NOT NULL;
     """
 
@@ -43,24 +49,34 @@ if __name__ == "__main__":
     fig = px.box(dfAnalysis.loc[selected_rows], x="population_name", y="percentage", color="response")
     st.plotly_chart(fig, theme="streamlit", width="stretch")
 
-    dfAnalysis["binary_response"] = dfAnalysis["response"] == "yes"
+    dfAnalysis["binary_response"] = dfAnalysis['response'].map({'yes':1, 'no':0})
+    
     
     for population in dfAnalysis["population_name"].unique():
         print(population)
         data = dfAnalysis.loc[dfAnalysis["population_name"] == population]
         fam = sm.families.Binomial()
         ind = sm.cov_struct.Exchangeable()
-        model = smf.gee("response ~ percentage".format(population), "sample_id", data, 
+        model = smf.gee("binary_response ~ percentage + time_from_treatment_start".format(population), "sample_id", data, 
                       cov_struct=ind, family=fam)
         
         result = model.fit()
         print(result.summary())
 
-        model = smf.gee("response ~ age + sex + percentage".format(population), "sample_id", data, 
+        model = smf.gee("binary_response ~ percentage + age + sex".format(population), "sample_id", data, 
                       cov_struct=ind, family=fam)
         
         result = model.fit()
         print(result.summary())
+
+
+        gp_model = gpb.GPModel(group_data=data["sample_id"], likelihood="binary")
+        x = sm.tools.tools.add_constant(data["percentage"])
+
+        result = gp_model.fit(y=data["binary_response"], X=x)
+        print(result.summary())
+        
+        
 
 
 # use t tests to start but this might need linear mixed models
