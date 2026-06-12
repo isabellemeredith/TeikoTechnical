e19d04a94d0825f3716137480751aff7f5876625
isabellemeredith <isabellejmeredith@gmail.com>
2026-06-11 23:04:38 -0600

    Fix the overview to use the new database schema

--- data_overview.py
+++ data_overview.py
@@ -3,7 +3,7 @@ import pandas as pd
 import sqlite3
 
 
-@st.cache_data(scope="session")
+@st.cache_data()
 def get_data(data_query, _connection):
     df = pd.read_sql_query(data_query, _connection)
     return df
@@ -18,83 +18,17 @@ if __name__ == "__main__":
     cursor = connection.cursor()
 
     summary_view_query = """
-        SELECT SampleIDName, 
-               b_Cell + cd8_T_Cell + cd4_T_Cell + nk_cell + monocyte,
-               CASE FROM SAMPLE;
-    """
-
-    summary_view_query = """
-        SELECT *
-        FROM 
-        (
-            SELECT SampleIDName, population,
-            CASE population
-            WHEN 'b_Cell' then b_Cell
-            WHEN 'cd8_T_Cell' then cd8_T_Cell
-            WHEN 'cd4_T_Cell' then cd4_T_Cell
-            WHEN 'nk_cell' then nk_cell
-            WHEN 'monocyte' then monocyte
-            END AS count
-        FROM SAMPLE
-        CROSS JOIN (VALUES('b_Cell'),('cd8_T_Cell'),('cd4_T_Cell'),('nk_cell'),('monocyte')) AS Pop(Population)
-        ) AS D
-        WHERE count IS NOT NULL;
-    """
-
-    
-
-    # summary_view_query = """
-    #     SELECT SampleIDName AS sample, population,
-    #         CASE t.column1 
-    #             WHEN 'b_Cell' then b_Cell
-    #             WHEN 'cd8_T_Cell' then cd8_T_Cell
-    #             WHEN 'cd4_T_Cell' then cd4_T_Cell
-    #             WHEN 'nk_cell' then nk_cell
-    #             WHEN 'monocyte' then monocyte
-    #         END AS population,
-    #         CASE t.column1 WHEN 'b_Cell' then b_Cell.value1 WHEN 2 THEN  original_table.value2 WHEN 3 THEN  original_table.value3 ELSE  original_table.value4 END AS value
-
-    #     from SAMPLE
-    #     JOIN (VALUES('b_Cell'),('cd8_T_Cell'),('cd4_T_Cell'),('nk_cell'),('monocyte')) t
-    #     )as D
-    #     where marks is not null;
-    # """
-
-    summary_view_query = """
-        SELECT original_table._id,
-          CASE t.column1 WHEN 1 THEN 'value1' WHEN 2 THEN 'value2' WHEN 3 THEN 'value3' ELSE 'value4' END AS name, 
-          CASE t.column1 WHEN 1 THEN  original_table.value1 WHEN 2 THEN  original_table.value2 WHEN 3 THEN  original_table.value3 ELSE  original_table.value4 END AS value
-        FROM (
-        select 1 _id, 10 value1 , 11 value2 , "string value" value3 , 13 value4
-        union all 
-        select 2, 14,  "string value",null,17
-        ) original_table
-        JOIN (VALUES(1),(2),(3),(4)) t
-    """
-
-    summary_view_query = """
-        SELECT * FROM SAMPLE JOIN (VALUES('b_Cell'),('cd8_T_Cell'),('cd4_T_Cell'),('nk_cell'),('monocyte')) t
-    """
-
-    summary_view_query = """
-        CREATE VIEW populations 
+        CREATE VIEW populations_summary 
         AS
-        SELECT sample, population, total_count, count, 1.0 * count / total_count AS percentage FROM (
-        SELECT SampleIDName AS sample, b_Cell + cd8_T_Cell + cd4_T_Cell + nk_cell + monocyte AS total_count, 'b_Cell' AS population,  b_Cell AS count FROM SAMPLE
-        UNION ALL
-        SELECT SampleIDName AS sample, b_Cell + cd8_T_Cell + cd4_T_Cell + nk_cell + monocyte AS total_count, 'cd8_T_Cell' AS population, cd8_T_Cell AS count FROM SAMPLE
-        UNION ALL
-        SELECT SampleIDName AS sample, b_Cell + cd8_T_Cell + cd4_T_Cell + nk_cell + monocyte AS total_count, 'cd4_T_Cell' AS population, cd4_T_Cell AS count FROM SAMPLE
-        UNION ALL
-        SELECT SampleIDName AS sample, b_Cell + cd8_T_Cell + cd4_T_Cell + nk_cell + monocyte AS total_count, 'nk_cell' AS population, nk_cell AS count FROM SAMPLE
-        UNION ALL
-        SELECT SampleIDName AS sample, b_Cell + cd8_T_Cell + cd4_T_Cell + nk_cell + monocyte AS total_count, 'monocyte' AS population, monocyte AS count FROM SAMPLE);
+        SELECT sample_id_text AS sample, population_name AS population, SUM(count) OVER (PARTITION BY sample_id) AS total_count, count, 1.0 * count / SUM(count) OVER (PARTITION BY sample_id) AS percentage FROM POPULATION;
     """
-    
+    cursor.execute("DROP VIEW IF EXISTS populations_summary;")
+    cursor.execute(summary_view_query)
+
     # cursor.execute("SELECT * FROM populations ORDER BY sample")
     # result = cursor.fetchall()
     # print(result)
-    get_population_data_query = "SELECT * FROM populations ORDER BY sample;"
+    get_population_data_query = "SELECT * FROM populations_summary ORDER BY sample;"
     dfPopulation = get_data(get_population_data_query, connection)
 
     print(dfPopulation.head())
