d2609596d5cf6232c08e14c51837dfabc0e72c29
isabellemeredith <isabellejmeredith@gmail.com>
2026-06-12 16:33:03 -0600

    Update dashboard to use new pages subfolder

--- dashboard.py
+++ dashboard.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
 if __name__ == "__main__":
-    data_overview = st.Page("data_overview.py", title="Data Overview") # , icon="🎈"
-    statistical_analysis = st.Page("statistical_analysis.py", title="Analysis") # , icon="❄️"
-    subset_analysis = st.Page("subset_analysis.py", title="Subset Analysis") # , icon="🎉"
+    data_overview = st.Page("./pages/data_overview.py", title="Data Overview") # , icon="🎈"
+    statistical_analysis = st.Page("./pages/statistical_analysis.py", title="Analysis") # , icon="❄️"
+    subset_analysis = st.Page("./pages/subset_analysis.py", title="Subset Analysis") # , icon="🎉"
 
     # Set up navigation
     pg = st.navigation([data_overview, statistical_analysis, subset_analysis])
