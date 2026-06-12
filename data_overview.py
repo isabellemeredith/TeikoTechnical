47b22d57dec1df042e4ebd1908b4daf835014752
isabellemeredith <isabellejmeredith@gmail.com>
2026-06-12 10:02:49 -0600

    Refactored the overview pagination code as a util in order to reuse it for the subset analysis later

--- data_overview.py
+++ data_overview.py
@@ -1,17 +1,8 @@
 import streamlit as st
 import pandas as pd
 import sqlite3
+import utils.display as display
 
-
-@st.cache_data()
-def get_data(data_query, _connection):
-    df = pd.read_sql_query(data_query, _connection)
-    return df
-
-@st.cache_data(show_spinner=False)
-def split_frame(input_df, rows):
-    df = [input_df.loc[i : i + rows - 1, :] for i in range(0, len(input_df), rows)]
-    return df
 
 if __name__ == "__main__":
     connection = sqlite3.connect("cell-count.db")
@@ -29,50 +20,14 @@ if __name__ == "__main__":
     # result = cursor.fetchall()
     # print(result)
     get_population_data_query = "SELECT * FROM populations_summary ORDER BY sample;"
-    dfPopulation = get_data(get_population_data_query, connection)
-
-    print(dfPopulation.head())
+    dfPopulation = display.get_data(get_population_data_query, connection)
 
     st.title("Data Overview")
 
+    display.display_paginated_dataframe(dfPopulation)
+
     # st.dataframe(dfPopulation)
 
-    top_menu = st.columns(3)
-    with top_menu[0]:
-        sort = st.radio("Sort Data", options=["Yes", "No"], horizontal=1, index=1)
-    if sort == "Yes":
-        with top_menu[1]:
-            sort_field = st.selectbox("Sort By", options=dfPopulation.columns)
-        with top_menu[2]:
-            sort_direction = st.radio(
-                "Direction", options=["Asc", "Desc"], horizontal=True
-            )
-        dfPopulation = dfPopulation.sort_values(
-            by=sort_field, ascending=sort_direction == "Asc", ignore_index=True
-        )
-    pagination = st.container()
-
-    bottom_menu = st.columns((4, 1, 1))
-    with bottom_menu[2]:
-        batch_size = st.selectbox("Page Size", options=[25, 50, 100, 500, "All"])
-        if batch_size == "All":
-            batch_size = len(dfPopulation)
-            
-    with bottom_menu[1]:
-        total_pages = (
-            int(len(dfPopulation) / batch_size) if int(len(dfPopulation) / batch_size) > 0 else 1
-        )
-        if ( batch_size * total_pages ) < len(dfPopulation):
-            total_pages += 1
-        current_page = st.number_input(
-            "Page", min_value=1, max_value=total_pages, step=1
-        )
-    with bottom_menu[0]:
-        st.markdown(f"Page **{current_page}** of **{total_pages}** ")
-
-
-
-    pages = split_frame(dfPopulation, batch_size)
-    pagination.dataframe(data=pages[current_page - 1], width='stretch')
+    
 
 
---
+++ utils/display.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import pandas as pd
+
+
+@st.cache_data()
+def get_data(data_query, _connection):
+    df = pd.read_sql_query(data_query, _connection)
+    return df
+
+@st.cache_data(show_spinner=False)
+def split_frame(input_df, rows):
+    df = [input_df.loc[i : i + rows - 1, :] for i in range(0, len(input_df), rows)]
+    return df
+
+def display_paginated_dataframe(data):
+    top_menu = st.columns(3)
+    with top_menu[0]:
+        sort = st.radio("Sort Data", options=["Yes", "No"], horizontal=1, index=1)
+    if sort == "Yes":
+        with top_menu[1]:
+            sort_field = st.selectbox("Sort By", options=data.columns)
+        with top_menu[2]:
+            sort_direction = st.radio(
+                "Direction", options=["Asc", "Desc"], horizontal=True
+            )
+        data = data.sort_values(
+            by=sort_field, ascending=sort_direction == "Asc", ignore_index=True
+        )
+    pagination = st.container()
+
+    bottom_menu = st.columns((4, 1, 1))
+    with bottom_menu[2]:
+        batch_size = st.selectbox("Page Size", options=[25, 50, 100, 500, "All"])
+        if batch_size == "All":
+            batch_size = len(data)
+            
+    with bottom_menu[1]:
+        total_pages = (
+            int(len(data) / batch_size) if int(len(data) / batch_size) > 0 else 1
+        )
+        if ( batch_size * total_pages ) < len(data):
+            total_pages += 1
+        current_page = st.number_input(
+            "Page", min_value=1, max_value=total_pages, step=1
+        )
+    with bottom_menu[0]:
+        st.markdown(f"Page **{current_page}** of **{total_pages}** ")
+
+
+
+    pages = split_frame(data, batch_size)
+    pagination.dataframe(data=pages[current_page - 1], width='stretch')
